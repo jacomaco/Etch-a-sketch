98a2fd329c62a0ead76b344d409e4c98592bcf0c
Jacob Johansson <jacobknuts@gmail.com>
2026-01-11 22:51:05 +0100

    Refactor code for consistency in string quotes and improve formatting

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let gridSize;
-let currentMode = 'black'; // default mode: black
+let currentMode = "black"; // default mode: black
 
 const clearBtn = document.getElementById("clearBtn");
 const enterGridSizeBtn = document.getElementById("enterBtn");
 const rGBButton = document.getElementById("specialBtn");
-const eraserBtn = document.getElementById("eraserBtn"); 
-const blackBtn = document.getElementById("blackBtn"); 
-
+const eraserBtn = document.getElementById("eraserBtn");
+const blackBtn = document.getElementById("blackBtn");
 
 initializeBoard();
 
 blackBtn.addEventListener("click", () => {
-    currentMode = 'black'; 
+    currentMode = "black";
 });
 
 enterGridSizeBtn.addEventListener("click", () => {
@@ -20,22 +19,21 @@ enterGridSizeBtn.addEventListener("click", () => {
 
 clearBtn.addEventListener("click", () => {
     const cells = document.querySelectorAll(".cell");
-    cells.forEach(cell => cell.style.background = "#d3d3d3");
+    cells.forEach((cell) => (cell.style.background = "#d3d3d3"));
 });
 
 rGBButton.addEventListener("click", () => {
-    currentMode = 'rainbow'; // Set mode to rainbow
+    currentMode = "rainbow"; // Set mode to rainbow
 });
 
 eraserBtn.addEventListener("click", () => {
-    currentMode = 'eraser'; // Set mode to eraser
+    currentMode = "eraser"; // Set mode to eraser
 });
 
-// You might want a button to go back to black, 
-// or you can make the buttons toggle. For now, let's assume 
-// clicking "Clear" or a "Black" button sets it back, 
-// or simply add a "Black Pen" button. 
-
+// You might want a button to go back to black,
+// or you can make the buttons toggle. For now, let's assume
+// clicking "Clear" or a "Black" button sets it back,
+// or simply add a "Black Pen" button.
 
 // --- THE LOGIC ---
 
@@ -58,9 +56,9 @@ function initializeBoard() {
 
         // CHECK THE MODE VARIABLE ON HOVER
         div.addEventListener("mouseenter", (e) => {
-            if (currentMode === 'rainbow') {
+            if (currentMode === "rainbow") {
                 e.currentTarget.style.background = getRandomColor();
-            } else if (currentMode === 'eraser') {
+            } else if (currentMode === "eraser") {
                 e.currentTarget.style.background = "#d3d3d3"; // Eraser just paints white!
             } else {
                 e.currentTarget.style.background = "black"; // Default
@@ -72,10 +70,10 @@ function initializeBoard() {
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
