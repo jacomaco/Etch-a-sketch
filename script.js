0495ed23641e840b7ce63224d6eca01d75b93b95
Jacob Johansson <jacobknuts@gmail.com>
2026-01-11 18:36:10 +0100

    Added functionality so that the grid is dynamicaly resizeable

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+let gridSize;
+while (true) {
+    gridSize = Number(prompt("How large grid? (enter a number from 1 to 100?)"));
+    if (gridSize >= 1 && gridSize <= 100) {
+        break;
+    }
+}
 const board = document.querySelector(".board");
+let boardSize = gridSize * gridSize;
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < boardSize; i++) {
     let div = document.createElement("div");
     div.classList.add("cell");
+    div.style.width = `calc(100% / ${gridSize})`;
+    div.style.height = `calc(100% / ${gridSize})`;
     board.appendChild(div);
 }
